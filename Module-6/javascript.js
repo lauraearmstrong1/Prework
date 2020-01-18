01869659fede27320bef0c1fc418602b0af677cd
Laura Armstrong <lauraearmstrong1@gmail.com>
2020-01-18 12:14:06 -0500

    added prework

---
+++ Module-6/javascript.js
@@ -0,0 +1,36 @@
+
+
+        document.getElementById("button1").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "250px";
+
+        });
+
+        document.getElementById("button2").addEventListener("click", function(){
+
+            document.getElementById("box").style.backgroundColor = "blue";
+
+        });
+
+        document.getElementById("button3").addEventListener("click", function(){
+
+            document.getElementById("box").style.opacity = "0.5";
+
+        });
+
+        document.getElementById("button4").addEventListener("click", function(){
+
+            document.getElementById("box").style.height = "150px";
+            document.getElementById("box").style.backgroundColor = "orange" ; 
+            document.getElementById("box").style.width = "150px" ; 
+            document.getElementById("box").style.margin = "25px";
+            document.getElementById("box").style.opacity = "1";
+
+            /*style="height:150px; width:150px; background-color:orange; margin:25px"
+            
+            
+            
+            CAN I RESET WITHOUT THE MULTIPLE DOCUMENTS??*/
+
+        });
+
